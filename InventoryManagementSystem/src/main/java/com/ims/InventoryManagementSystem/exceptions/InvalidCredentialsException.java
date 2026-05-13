aa073788f0916019de4eda11261b0a6ba25844a2
amirhatam <amirhatam136@gmail.com>
2026-05-13 10:25:51 +0200

    Add Invalid Credentials Exception

---
+++ InventoryManagementSystem/src/main/java/com/ims/InventoryManagementSystem/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.ims.InventoryManagementSystem.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
